bb5b65f792a23f8d4979f3202fccc2e48a431c7f
dpq <1692303173@qq.com>
2021-08-20 10:01:07 +0800

    添加datastore路由

--- jetpack/datastore/src/main/java/com/herdin/android/datastore/MainActivity.kt
+++ jetpack/datastore/src/main/java/com/herdin/android/datastore/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.herdin.android.datastore
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.herdin.android.base.RouterPage
 import com.herdin.android.base.activty.BaseActivity
 import com.herdin.android.base.activty.RootActivity
 
+@Route(path = RouterPage.DATASTORE_MAIN)
 class MainActivity : RootActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
